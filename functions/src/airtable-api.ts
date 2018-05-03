8e1cbee2a01e09424f1d80c021f1c77f84b98c91
Daniel Lewis <dan@rootsystems.nz>
2018-05-03 14:11:05 +1200

    bring back the linter

--- functions/src/airtable-api.ts
+++ functions/src/airtable-api.ts
@@ -1,31 +1,29 @@
 import * as functions from 'firebase-functions';
 import * as Airtable from "airtable"
 
-export module AirtableApi {
-  const base = new Airtable({apiKey: functions.config().airtable.api_key}).base(functions.config().airtable.base);
+const base = new Airtable({apiKey: functions.config().airtable.api_key}).base(functions.config().airtable.base);
 
-  export function GetPeople():any {
-    const people = []
-    base('Person').select({
-        view: "Website View"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-        people.push(records)
-    
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
-        
-    }, function done(err) {
-        if (err) {console.error('Error: ', err); return;}
-        console.log(people)
-        return people
-    });
-  }
+export function GetPeople():any {
+  const people = []
+  base('Person').select({
+      view: "Website View"
+  }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+      people.push(records)
   
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+      
+  }, function done(err) {
+      if (err) {console.error('Error: ', err); return;}
+      console.log(people)
+      return people
+  });
+}
 
-  export function GetVentures() {
-    //Todo
-  }
+
+export function GetVentures() {
+  //Todo
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,10 @@
 import * as functions from 'firebase-functions';
-import { AirtableApi } from './airtable-api';
+import { GetPeople } from './airtable-api';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
 export const getPeople = functions.https.onRequest((request, response) => {
-  response.send(AirtableApi.GetPeople())
+  response.send(GetPeople())
 });
