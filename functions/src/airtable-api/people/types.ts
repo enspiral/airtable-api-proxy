1a158cc90c3b20d030e8274fdd8161966ccce190
Daniel Lewis <dan@rootsystems.nz>
2018-05-08 07:48:01 +1200

    extend types for Ambassador

--- functions/src/airtable-api/people/types.ts
+++ functions/src/airtable-api/people/types.ts
@@ -4,4 +4,32 @@ export const Person = t.interface({
   name: t.string
 })
 
+export const Ambassador = t.interface({
+  name: t.string,
+  description: t.string,
+  quote: t.string,
+  email: t.string,
+  linkedin: t.string,
+  twitter: t.string,
+  gravatarUrl: t.string,
+  imageUrl: t.string,
+  ssb: t.string,
+  venture: t.string,
+  ventureUrl: t.string,
+  role: t.string
+})
+
+export const ArrayOfAmbassador = t.interface({
+  ambassadors: t.array(Ambassador)
+})
+
+export const Contributor = t.interface({
+  id: t.string,
+  name: t.string,
+  gravatarUrl: t.string,
+  quote: t.string
+})
+
 export type IPerson = t.TypeOf<typeof Person>
+export type IAmbassador = t.TypeOf<typeof Ambassador>
+export type IContributor = t.TypeOf<typeof Contributor>
