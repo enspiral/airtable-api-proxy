20b8a6a2ebacf7a8af7bef10c264f31aebd9807d
Daniel Lewis <dan@rootsystems.nz>
2018-05-08 18:37:21 +1200

    refactor and deramdafy with james and jo!

--- functions/src/airtable-api/people/index.ts
+++ functions/src/airtable-api/people/index.ts
@@ -9,44 +9,46 @@ const logger = pino()
 export function GetAmbassadors() {
     return new Promise((resolve, reject) => {
         GetAirtablePersons()
-        .then((persons :Array<any>) => {
-            // Todo: flatten fields values to be with createdTime and id
-            resolve(forEach(flattenPerson,persons))
+        .then((persons) => {
+            return filterPersonsProfiles(persons)
         })
-        .catch(err => {console.error('Error: ', err); reject(err);})
+        .then((persons) => {
+            // Todo: flatten fields values to be with createdTime and id
+            resolve(persons.map(flattenPerson))
+        })
+        .catch(err => {console.error('Error: ', err); reject(err)})
     })
 }
 
 function GetAirtablePersons() {
-  return new Promise((resolve, reject) => {
-    const airtablePersons :Array<string> = []
+    return new Promise((resolve, reject) => {
+        const airtablePersons :Array<string> = []
 
-    // DL: Name of Table in Airtable base (db)
-    base('Person').select({
+        // DL: Name of Table in Airtable base (db)
+        base('Person').select({
 
-        // DL: Name of View of Table
-        view: "Website View"
+            // DL: Name of View of Table
+            view: "Website View"
 
-    // DL: Airtable returns paginated views - here we accumulate them into one object
-    }).eachPage(function page(records, fetchNextPage) {
-        airtablePersons.push(records)
-        fetchNextPage();
+        // DL: Airtable returns paginated views - here we accumulate them into one object
+        }).eachPage(function page(records, fetchNextPage) {
+            airtablePersons.push(records)
+            fetchNextPage();
 
-    }, function done(err) {
-        if (err) {console.error('Error: ', err); reject(err);}
-        filterPersonsProfiles(airtablePersons, resolve)
+        }, function done(err) {
+            if (err) {console.error('Error: ', err); reject(err);}
+            resolve(airtablePersons)
+        })
     })
-  })
 }
 
 const getFields = prop('fields')
 const getMetaData = pick(['id', 'createdTime'])
 
 // Todo: test
-function filterPersonsProfiles (airtablePersons, resolve) :any {
+function filterPersonsProfiles (airtablePersons) {
     // DL: Plucking the _rawJson here removes all meta-data except id and created-at
-    const persons = pluck('_rawJson')(flatten(airtablePersons))
-    resolve(persons)
+    return pluck('_rawJson')(flatten(airtablePersons))
 }
 
 // Todo: test
