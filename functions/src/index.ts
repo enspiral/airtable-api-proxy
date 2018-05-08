8cb5276c183578dc53272ee8e0ae71b33ed8fa05
Daniel Lewis <dan@rootsystems.nz>
2018-05-08 18:36:48 +1200

    promisify with logger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from 'firebase-functions'
+import * as pino from 'pino'
 import { GetAmbassadors } from './airtable-api/people'
+
+const logger = pino()
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!")
@@ -7,7 +10,11 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
 export const getPeople = functions.https.onRequest((request, response) => {
   GetAmbassadors()
-  .then((data) => {
-    response.send(data)
+  .then(data => {
+    response.status(200).send(data)
+  })
+  .catch(err => {
+    logger.error(err, 'Get people error')
+    response.status(400)
   })
 })
