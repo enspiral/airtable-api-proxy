dbd6ad5675c8ac05b5740aff2ad4d6209278d5dc
Daniel Lewis <dan@rootsystems.nz>
2018-05-10 15:42:04 +1200

    add catch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,18 @@
-import * as functions from 'firebase-functions';
-import { GetPersons } from './airtable-api/people';
+import * as functions from 'firebase-functions'
+import * as pino from 'pino'
+import { GetPersons } from './airtable-api/people'
+
+const logger = pino()
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+ response.send("Hello from Firebase!")
+})
 
 export const getPeople = functions.https.onRequest((request, response) => {
   GetPersons()
   .then((data) => {
     response.send(data)
+  }).catch((err) => {
+    logger.error(err)
   })
-});
+})
