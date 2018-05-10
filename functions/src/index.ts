2322b9410d84cc5569bd1f318f41c5983207b8d4
Daniel Lewis <dan@rootsystems.nz>
2018-05-10 16:10:08 +1200

    revert to previous commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,20 @@
 import * as functions from 'firebase-functions'
 import * as pino from 'pino'
-import { GetPersons } from './airtable-api/people'
+import {GetPersons() } from './airtable-api/people'
 
 const logger = pino()
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!")
-})
+});
 
 export const getPeople = functions.https.onRequest((request, response) => {
   GetPersons()
-  .then((data) => {
-    response.send(data)
-  }).catch((err) => {
-    logger.error(err)
+  .then(data => {
+    response.status(200).send(data)
+  })
+  .catch(err => {
+    logger.error(err, 'Get people error')
+    response.status(400)
   })
 })
