69c1588eea90ac1fd92b2eeec905575b5091da1c
Daniel Lewis <dan@rootsystems.nz>
2018-05-10 19:24:11 +1200

    add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,24 @@
 import * as functions from 'firebase-functions'
+import * as cors from 'cors'
+
 import { GetPersons } from './airtable-api/people'
+
+const corsHandler = cors({origin: true})
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!")
 });
 
 export const getPeople = functions.https.onRequest((request, response) => {
-  GetPersons()
-  .then(data => {
-    console.log('Return Flattened Persons Profiles: ', data)
-    response.status(200).send(data)
-  })
-  .catch(err => {
-    console.error('GetPeople Errored: ', err)
-    response.status(400)
+  corsHandler(request, response, () => {
+    GetPersons()
+    .then(data => {
+      console.log('Return Flattened Persons Profiles: ', data)
+      response.status(200).send(data)
+    })
+    .catch(err => {
+      console.error('GetPeople Errored: ', err)
+      response.status(400)
+    })
   })
 })
