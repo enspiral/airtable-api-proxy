a2d108cb88ee5dc3bd6a0317bab24aa5ec7d9949
Daniel Lewis <dan@rootsystems.nz>
2018-05-11 12:07:08 +1200

    update .ts to js

--- functions/src/airtable-api/airtable.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from 'firebase-functions'
-import * as airtable from 'airtable'
-
-export default new airtable({apiKey: functions.config().airtable.api_key}).base(functions.config().airtable.base)
--- functions/src/airtable-api/ventures/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Functions from 'firebase-functions'
-import * as Airtable from "airtable"
-
-const base = new Airtable({apiKey: Functions.config().airtable.api_key}).base(Functions.config().airtable.base);
-
-export function GetVentures() {
-  //Todo
-}
