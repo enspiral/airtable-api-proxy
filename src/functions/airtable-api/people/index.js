a6a4fd8035d0692ff1775aacce567864e6f804a5
Daniel Lewis <dan@rootsystems.nz>
2018-05-12 01:31:25 +1200

    ramda camelcasify

--- src/functions/airtable-api/people/index.js
+++ src/functions/airtable-api/people/index.js
@@ -1,13 +1,36 @@
-import { flatten, map, pick, pluck, pipe, prop } from 'ramda'
-import { spreadProp } from 'ramda-adjunct'
+import {
+  either,
+  flatten,
+  ifElse,
+  is,
+  identity,
+  map,
+  pluck,
+  pipe,
+  replace,
+  trim
+} from 'ramda'
+import { spreadProp, renameKeysWith } from 'ramda-adjunct'
+import camelcase from 'camelcase'
 import base from '../airtable'
 
 // Data transforms
 // Todo: test
 const getRawJson = pluck('_rawJson')
-
 export const filterPersonsProfiles = pipe(flatten, getRawJson)
+
 export const flattenPersonsProfile = map(spreadProp('fields'))
+
+export const replaceNonAlphaWithSpace = replace(/( ?[^a-zA-Z\d\s:]? )/g, ' ')
+const tidyKey = pipe(replaceNonAlphaWithSpace, trim, camelcase)
+
+export const cleanKey = renameKeysWith(tidyKey)
+
+export const cleanAndCamelKeys = ifElse(
+  either(is(Array), is(Object)),
+  pipe(cleanKey, map(a => cleanAndCamelKeys(a))),
+  identity
+)
 
 // Driver code
 export function GetPersons () {
@@ -18,12 +41,15 @@ export function GetPersons () {
     .then(personsProfiles => {
       return flattenPersonsProfile(personsProfiles)
     })
+    .then(flatPersonsProfiles => {
+      return cleanAndCamelKeys(flatPersonsProfiles)
+    })
 }
 
 // Api Request
 function GetAirtablePersons () {
   return new Promise((resolve, reject) => {
-    console.log('GetAirTablePersons Started')
+    console.info('GetAirTablePersons Started')
     const airtablePersons = []
 
     // DL: Name of Table in Airtable base (db)
@@ -31,20 +57,22 @@ function GetAirtablePersons () {
       .select({
         // DL: Name of View of Table
         view: 'Website View'
-
-        // DL: Airtable returns paginated views - here we accumulate them into one object
       })
       .eachPage(
         (records, fetchNextPage) => {
+          // DL: Airtable returns paginated views - here we accumulate them into one object
           airtablePersons.push(records)
           fetchNextPage()
         },
         err => {
           if (err) {
-            console.log('Airtable Request Error: ', err)
+            console.error(message, optionalParams)(
+              'Airtable Request Error: ',
+              err
+            )
             reject(err)
           }
-          console.log('GetAirtablePersos Successful: ', airtablePersons)
+          console.info('GetAirtablePersos Successful: ')
           resolve(airtablePersons)
         }
       )
