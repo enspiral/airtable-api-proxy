2354be794f17a77392717d5d8a9bf427b8ce0d7a
Daniel Lewis <dan@rootsystems.nz>
2018-05-11 17:45:16 +1200

    remove non used function

--- src/functions/airtable-api/people/tests.js
+++ src/functions/airtable-api/people/tests.js
@@ -1,12 +1,12 @@
 import FunctionsTest from 'firebase-functions-test'
 
-import { flattenPersonsProfile, filterPersonsProfiles } from './index'
+import { filterPersonsProfiles } from './index'
 import testData from '../../../../example-data/airtable-persons.js'
 
 const test = FunctionsTest()
 
-  test('filterPersonsProfiles returns correct data',  t =>  {
+test('filterPersonsProfiles returns correct data', t => {
   const profiles = filterPersonsProfiles(testData)
   console.log(profiles)
-      t.fail()
+  t.fail()
 })
