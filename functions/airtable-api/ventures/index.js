4e9f63901b38ece0f7b1b3beae2d3888096a1741
Daniel Lewis <dan@rootsystems.nz>
2018-05-11 13:59:04 +1200

    update import syntax

--- functions/airtable-api/ventures/index.js
+++ functions/airtable-api/ventures/index.js
@@ -1,7 +1,7 @@
-import Functions from 'firebase-functions'
+import * as functions from 'firebase-functions'
 import Airtable from "airtable"
 
-const base = new Airtable({apiKey: Functions.config().airtable.api_key}).base(Functions.config().airtable.base);
+const base = new Airtable({apiKey: functions.config().airtable.api_key}).base(functions.config().airtable.base);
 
 export function GetVentures() {
   //Todo
