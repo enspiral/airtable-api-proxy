75a85532262a15ecd7dcb5be8a3257eee29a37f3
Daniel Lewis <dan@rootsystems.nz>
2018-05-13 15:33:51 +1200

    add mapKeys and update utilities  comments

--- src/functions/utility.js
+++ src/functions/utility.js
@@ -1,4 +1,4 @@
-import { chain, curry, either, ifElse, is, identity, map, pipe, replace, trim, zipObj } from 'ramda'
+import { adjust, chain, curry, either, fromPairs, ifElse, is, identity, map, pipe, replace, toPairs, trim, zipObj } from 'ramda'
 import { renameKeysWith } from 'ramda-adjunct'
 import camelcase from 'camelcase'
 import md5 from 'js-md5'
@@ -26,7 +26,18 @@ export const computeGravatarUrl = email => {
   }
 }
 
-// Make an object out of a list, with keys derived form each element
-// https://github.com/ramda/ramda/wiki/Cookbook#make-an-object-out-of-a-list-with-keys-derived-form-each-element
+/* Make an object out of a list, with keys derived form each element
+ * https://github.com/ramda/ramda/wiki/Cookbook#make-an-object-out-of-a-list-with-keys-derived-form-each-element
+ * objFromListWith(prop('id'),[{ id: 'foo', name: 'John' }, { id: 'bar', name: 'Jane' }])
+ * // { foo: { id: 'foo', name: 'John' }, bar: { id: 'bar', name: 'Jane' } }
+ */
 
 export const objFromListWith = curry((fn, list) => chain(zipObj, map(fn))(list))
+
+/* Map keys of an object
+ * https://github.com/ramda/ramda/wiki/Cookbook#map-keys-of-an-object
+ * mapKeys(R.toUpper, { a: 1, b: 2, c: 3 })
+ * // { A: 1, B: 2, C: 3 }
+ */
+
+export const mapKeys = curry((fn, obj) => fromPairs(map(adjust(fn, 0), toPairs(obj))))
