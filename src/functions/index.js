638314dcbcd80fdd0df3f604a718ff9726c44f14
Daniel Lewis <dan@rootsystems.nz>
2018-05-14 00:26:04 +1200

    - add firebase-admin and save data to db  - update function name from getPeople to updatepersons to reflect the new functionality  - stop returning the data and instead return  success message

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,16 +1,24 @@
 import * as functions from 'firebase-functions'
 import cors from 'cors'
+import admin from 'firebase-admin'
 
 import { GetPersons } from './airtable-api/people'
 
+admin.initializeApp(functions.config().firebase)
+
 const corsHandler = cors({ origin: true })
 
-export const getPeople = functions.https.onRequest((request, response) => {
+export const updatepeople = functions.https.onRequest((request, response) => {
   corsHandler(request, response, () => {
     GetPersons()
+      .then(persons => {
+        console.info('Update: firebase database - Persons')
+        admin.database().ref('/persons').set(persons)
+        return persons
+      })
       .then(data => {
         console.info('Return Flattened Persons Profiles: ', data)
-        return response.status(200).send(data)
+        return response.status(200).send({message: 'Update Persons Successful'})
       })
       .catch(err => {
         console.error('GetPeople Errored: ', err)
