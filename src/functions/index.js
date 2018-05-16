09c7b41c671d7571f06be973d2b6a07075259f0b
Daniel Lewis <dan@rootsystems.nz>
2018-05-17 05:35:01 +1200

    add ventures update entry point

--- src/functions/index.js
+++ src/functions/index.js
@@ -2,7 +2,8 @@ import * as functions from 'firebase-functions'
 import cors from 'cors'
 import admin from 'firebase-admin'
 
-import { GetPersons } from './airtable-api/people'
+import { GetPersons } from './airtable-api/persons'
+import { GetVentures } from './airtable-api/ventures'
 
 admin.initializeApp(functions.config().firebase)
 
@@ -12,16 +13,35 @@ export const updatepeople = functions.https.onRequest((request, response) => {
   corsHandler(request, response, () => {
     GetPersons()
       .then(persons => {
-        console.info('Update: firebase database - Persons')
+        console.info('Update-Firebase-DB STARTED: <Persons>')
         admin.database().ref('/persons').set(persons)
         return persons
       })
       .then(data => {
-        console.info('Return Flattened Persons Profiles: ', data)
+        console.info('Update-Firebase-DB SUCCESFUL: <Persons>', data)
         return response.status(200).send({message: 'Update Persons Successful'})
       })
       .catch(err => {
-        console.error('GetPeople Errored: ', err)
+        console.error('GetPeople ERROR: ', err)
+        response.status(500)
+      })
+  })
+})
+
+export const updateventures = functions.https.onRequest((request, response) => {
+  corsHandler(request, response, () => {
+    GetVentures()
+      .then(ventures => {
+        console.info('Update-Firebase-DB STARTED: <Ventures>')
+        admin.database().ref('/ventures').set(ventures)
+        return ventures
+      })
+      .then(data => {
+        console.info('Update-Firebase-DB SUCCESFUL: <Ventures>', data)
+        return response.status(200).send({message: 'Update Ventures Successful'})
+      })
+      .catch(err => {
+        console.error('GetVentures ERROR: ', err)
         response.status(500)
       })
   })
